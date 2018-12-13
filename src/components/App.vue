32b9a8e02788edd9e68361be67ee7396a85e2c28
Shaba9 <shaba92@gmail.com>
2018-12-13 13:50:36 -0800

    app setup

---
+++ src/components/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app">
+    <RouterView></RouterView>
+    {{result}}
+  </div>
+</template>
+
+<script>
+
+import api from '../services/api';
+
+export default {
+  data() {
+    return {
+      result: null
+    };
+  },
+  created() {
+    api.test()
+      .then(result => this.result = result);
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
