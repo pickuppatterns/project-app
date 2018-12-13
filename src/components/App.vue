17bd8360ae8972eed6bc4a254a814db654be25d5
PDXCrow <piratehans@gmail.com>
2018-12-13 12:58:03 -0800

    app side built and working

---
+++ src/components/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <RouterView></RouterView>
+    {{result}}
+  </div>
+</template>
+
+<script>
+import api from '../services/api';
+export default {
+  data() {
+    return {
+      result: null
+    };
+  },
+  components: { 
+  },
+  created() {
+    api.test()
+      .then(result => this.result = result);
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
