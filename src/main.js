371202d944ef792f1a4f0d7fd02241308ebb998f
Paige Gorry <paigeegorry@gmail.com>
2018-12-13 11:37:59 -0800

    copy boilerplate and made adjustments

---
+++ src/main.js
@@ -0,0 +1,19 @@
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+import App from './components/App.vue';
+import router from './router';
+import './main.css';
+
+Vue.config.productionTip = false;
+Vue.use(VueRouter);
+
+Vue.directive('focus', {
+  inserted(el) {
+    el.focus();
+  }
+});
+
+new Vue({
+  router,
+  render: h => h(App)
+}).$mount('#app');
