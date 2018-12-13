322b26212b6bf86e26e0bef4ce31db0b1b04a77c
Paige Gorry <paigeegorry@gmail.com>
2018-12-13 11:50:56 -0800

    test api for server connection

--- src/components/App.vue
+++ src/components/App.vue
@@ -1,13 +1,24 @@
 <template>
   <div id="app">
     <RouterView></RouterView>
+
+    {{result}}
   </div>
 </template>
 
 <script>
+import api from '../services/api.js';
 export default {
+  data() {
+    return { 
+      result: null
+    };
+  },
   name: 'app',
   components: {
+  },
+  created() {
+    api.test().then(result => this.result = result);
   }
 };
 </script>
