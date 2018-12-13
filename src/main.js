e6c81d8a863301c2a298ca8c19804d3e96f978fd
Marcy Silverman <marcysilve225@gmail.com>
2018-12-13 12:34:20 -0800

    set up files

---
+++ src/main.js
@@ -0,0 +1,20 @@
+
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+import App from './components/App.vue';
+import router from './router';
+
+
+Vue.config.productionTip = false;
+Vue.use(VueRouter); 
+
+Vue.directive('focus', {
+  inserted(el) {
+    el.focus();
+  }
+});
+
+new Vue({
+  router,
+  render: h => h(App),
+}).$mount('#app');
---
+++ src/router.js
@@ -0,0 +1,9 @@
+import VueRouter from 'vue-router'; 
+import Home from './components/Home';  
+
+export default new VueRouter ({
+  routes : [
+    { path: '/', component: Home }, 
+    { path: '*', redirect: '/' }
+  ]
+});
