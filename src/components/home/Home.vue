a83b36c3af067cf21f4e5bee73d59990c01caae0
martypdx <marty@martypdx.com>
2018-12-14 09:43:10 -0800

    improve content copy  on Home page

--- src/components/home/Home.vue
+++ src/components/home/Home.vue
@@ -1,6 +1,9 @@
 <template>
   <section>
-    <h2>Welcome to My Project!</h2>
+    <h2>Welcome to Rocket Project!</h2>
+    <p>
+      Ruby Moltres Drifblim Frillish Drapion Kricketot Gligar. Grass Darumaka Poliwrath Zangoose Pikachu to train them is my cause Glitch City. Johto Golbat Hariyama Blizzard Totodile Cacturne Zangoose. V for victory Zigzagoon Rayquaza Kyogre Lillipup Ledian Cofagrigus. Fighting Ledyba Huntail Entei Diglett MysteryBerry Magikarp. 
+    </p>
   </section>
 </template>
 
