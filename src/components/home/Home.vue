9d8ab582d47fd1d030ccfcb95e7b3fd8e5a8c5cd
martypdx <marty@martypdx.com>
2018-12-14 10:11:11 -0800

    show restaurant data

--- src/components/home/Home.vue
+++ src/components/home/Home.vue
@@ -4,12 +4,23 @@
     <p>
       Ruby Moltres Drifblim Frillish Drapion Kricketot Gligar. Grass Darumaka Poliwrath Zangoose Pikachu to train them is my cause Glitch City. Johto Golbat Hariyama Blizzard Totodile Cacturne Zangoose. V for victory Zigzagoon Rayquaza Kyogre Lillipup Ledian Cofagrigus. Fighting Ledyba Huntail Entei Diglett MysteryBerry Magikarp. 
     </p>
+    {{restaurants}}
   </section>
 </template>
 
 <script>
+import restaurantApi from '../../services/restaurantApi';
+
 export default {
-  
+  data() {
+    return {
+      restaurants: null
+    };
+  },
+  created() {
+    restaurantApi.getAll()
+      .then(restaurants => this.restaurants = restaurants);
+  }
 };
 </script>
 
