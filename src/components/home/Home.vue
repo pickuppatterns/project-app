64d41f7d74c84cb7555244604aa71dcccffbb3dd
PDXCrow <piratehans@gmail.com>
2018-12-15 12:09:22 -0800

    modified copy on Home page

--- src/components/home/Home.vue
+++ src/components/home/Home.vue
@@ -1,6 +1,9 @@
 <template>
   <section>
-    <h2>Welcome to My Project!</h2>
+    <h2>Welcome to Joke Sample Project!</h2>
+    <p>
+        My dog used to chase people on a bike a lot. It got so bad I had to take his bike away.
+    </p>
   </section>
 </template>
 
